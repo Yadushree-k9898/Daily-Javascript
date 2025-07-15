0445c1e1f0468bdfede9bc27dd80431c0a447179
yadu <kyadushree47@gmail.com>
2025-07-15 13:16:59 +0530

    day1_js

---
+++ yadu/01_Day/01_day_starter/helloworld.js
@@ -0,0 +1,121 @@
+let firstName = 'yadushree';
+let lastName = 'k';
+let country = 'India';
+let city = 'Tumkur';
+let age = '100';
+
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14
+
+let name = 'yadu'
+age = 100
+live = 'India'
+console.log('Hello world');
+console.log('Welcome to 30days of javascript');
+
+console.log(firstName, lastName, country, city, age);
+console.log(gravity, boilingPoint, PI);
+console.log(name, live, age);
+
+
+//  x = 5
+//  y = 6
+//  z = x + y
+//  console.log(z);
+
+//  var x = 2
+//  var y =5
+//  var z = x +y
+//  console.log(z);
+
+
+//  let x = 5
+//  let y =3
+//  let z = x + y
+//  console.log(z);
+
+//  const x = 1
+//  const y = 2
+//  const z = x+y
+//  console.log(z);
+ 
+
+ 
+
+ const price1 = 5
+ const price2 = 3
+ const total = price1 + price2
+ console.log(total);
+ 
+/*
+ 
+1. Always declare variables
+2. Always use const if the value should not be changed
+3. Always use const if the type should not be changed (Arrays and Objects)
+4. Only use let if you can't use const
+5. Only use var if you MUST support old browsers.
+
+Creating a variable in JavaScript is called "declaring" a variable.
+
+*/
+
+
+let x = 5;
+let y = 6
+let z = x + y
+
+
+/*
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ (but we will not use it in this tutorial).
+Names are case sensitive (y and Y are different variables).
+Reserved words (like JavaScript keywords) cannot be used as names.
+
+*/
+
+
+const pi = 3.14
+let person2 = 'John doe'
+let answer = 'Yes I am!'
+
+
+var carName; //delaration
+let carName;
+
+
+carName = 'volvo';  //assign
+
+
+
+let person =  'John doe' , carName = 'volvo', price = 200
+
+
+let person1 = 'John doe'
+carName = 'Volvo'
+price = 200
+
+
+var carName = 'BMW';
+var carName; // this will work
+
+
+let carName = 'volvo';
+let carName; // this wont work in case of const also
+
+
+let x1 = 2 + 3+ 4
+console.log(x1);
+
+
+let x2 = 'John' + " " + "Doe"
+
+
+let $ = 'Hello world'
+let $$$ = 3;
+let $my_money = 5;
+
+let _name = 'john';
+let _x = 3;
+let _100 = 6;
