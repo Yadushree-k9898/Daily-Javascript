4f9e49f86b091affaa2bd03f182ed90395d2043e
NisargaArgekar <argekarnisarga015@gmail.com>
2025-07-15 23:03:53 +0530

    day-1 progress

---
+++ Nisarga/Day-1_Introduction/variable.js
@@ -0,0 +1,21 @@
+// Declaring different variables of different data types
+
+let firstName = 'Nisarga';
+let lastName = 'Argekar';
+let country = 'India';
+let city = 'Pune';
+let age = 100;
+let isMarried = true;
+
+// Declaring variables with number values
+
+const PI = 3.14;
+const gravity = 9.81;
+const boilingPoint = 100;
+const bodyTemperature = 37;
+
+// Variables can also be declaring in one line separated by comma
+
+let name='Nisa',
+    job='Software Engineer',
+    isEmployed=true;
