904405001b66b6cf0873a7c02fae345f72239e20
NisargaArgekar <argekarnisarga015@gmail.com>
2025-07-16 10:38:59 +0530

    Day-2

---
+++ Nisarga/Day-2/string.js
@@ -0,0 +1,17 @@
+let string = 'JavaScript'
+let firstLetter = string[0];
+console.log(firstLetter); // Output: J
+
+let secondLetter =string[1];
+console.log(secondLetter); // Output: a
+
+let thirdLetter = string[2];
+console.log(thirdLetter); // Output: v
+
+let lastLetter = string[9];
+console.log(lastLetter); // Output: t
+
+
+let lastIndex = string.length-1;
+console.log(lastIndex); // Output: 9
+console.log(string[lastIndex]); // Output: t
