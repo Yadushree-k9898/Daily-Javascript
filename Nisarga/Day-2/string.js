66d348defc55399eeecb4204388dad89817d04f9
NisargaArgekar <argekarnisarga015@gmail.com>
2025-07-16 23:48:09 +0530

    day_2

--- Nisarga/Day-2/string.js
+++ Nisarga/Day-2/string.js
@@ -26,3 +26,67 @@ console.log(string1.charAt(0))  // Output: 3
 
 let lastIndex1 = string1.length-1;
 console.log(string1.charAt(lastIndex1)); //output : t
+
+
+
+//  charCodeAt
+string.charCodeAt(index)
+let string2 = '30 Days of javascript'
+console.log(string2.charCodeAt(3)) // Output: 68 (ASCII code for 'D')
+let lastIndex2 = string2.length-1;
+console.log(string2.charCodeAt(lastIndex2)); // Output: 116 (ASCII code for 't') 
+
+
+//concat
+//  concat() -- two or more strings and join them together
+let n = '30';
+console.log(n.concat('days of javascript study')); // Output: 30days of javascript study
+
+
+//endsWith() --  it checks if a string ends with a specified string. and return value true or false
+ let  m = 'Do not Give up';
+ console.log(m.endsWith('up')); // Output: true
+ console.log(m.endsWith('Do')); // Output: false
+
+
+// startsWith() -- it checks if a string starts with a specified string. and return value true or false
+let p = 'Do not give up';
+console.log(p.startsWith('Do')); // Output: true
+console.log(p.startsWith('give')); // Output: false
+
+
+// includes() -- it checks specified string is present in a string or not . and return value true or false
+let q = 'Motivate yourself';
+console.log(q.includes('yourself')); // Output: true
+console.log(q.includes('your')); // Output: true
+
+
+// indexOf -- it returns the index of the first occurrence of a specified string in a string. If the specified string is not found, it returns -1.
+let r = 'Hello there!! 77'
+console.log(r.indexOf('there')); // Output: 6
+console.log(r.indexOf('hello')); // Output: -1
+console.log(r.indexOf(77)); // Output: 12
+
+//  lastIndexOf() -- it returns the index of the last occurrence of a specified string in a string. If the specified string is not found, it returns -1.
+let s = ' hello there!! 77 hello';
+console.log(s.lastIndexOf('hello')); // Output: 19
+console.log(s.lastIndexOf('7')); // Output: 20
+
+// length() -- it returns the length of a string.
+let t = 'Hello World';
+console.log(t.length); // Output: 11 includes spaces
+
+
+
+// match 
+// repeat 
+// match 
+// replace
+// search
+// split
+
+// substr 
+// substring 
+// toLowerCase
+// toUpperCase
+// trim
