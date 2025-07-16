d4e9f4dcad3f3c4a85fd17555a7c91f476ef690b
NisargaArgekar <argekarnisarga015@gmail.com>
2025-07-16 23:18:01 +0530

    day-2

--- Nisarga/Day-2/string.js
+++ Nisarga/Day-2/string.js
@@ -1,3 +1,6 @@
+
+// Accessing the Characters of a String ['JavaScript']
+
 let string = 'JavaScript'
 let firstLetter = string[0];
 console.log(firstLetter); // Output: J
@@ -15,3 +18,11 @@ console.log(lastLetter); // Output: t
 let lastIndex = string.length-1;
 console.log(lastIndex); // Output: 9
 console.log(string[lastIndex]); // Output: t
+
+//  charAt 
+string.charAt(index)
+let string1 = '30 days of javascript'
+console.log(string1.charAt(0))  // Output: 3
+
+let lastIndex1 = string1.length-1;
+console.log(string1.charAt(lastIndex1)); //output : t
---
+++ Nisarga/Day-2/stringDatatypes.js
@@ -0,0 +1,7 @@
+let space = '';
+let firstName = 'Nisarga';
+let secondName = 'Argekar';
+let country = 'India';
+let city = 'Mumbai';
+let language = 'JavaScript';
+let job = 'software Developer';
