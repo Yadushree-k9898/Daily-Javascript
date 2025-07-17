251481b39bec2fff4183870a7e607e75ad2ac110
NisargaArgekar <argekarnisarga015@gmail.com>
2025-07-17 23:57:13 +0530

    day_3

---
+++ Nisarga/Day-3/booleanOperators.js
@@ -0,0 +1,30 @@
+//  Boolean Operators
+
+let isLightOn = true 
+let isLightOff = false
+let trueValue = 7 > 5 // true
+let falseValue = 5 > 7 // false
+
+//  Truthy and Falsy values
+//  .. Truthy values are all numbers (+/-) are truthy expect 0
+// .. All strings are truthy except empty string
+//  all boolean objects are truthy
+
+// Falsy
+// 0
+// 0n
+// null
+let empty =  null;
+console.log(empty); // Output: null
+
+
+
+// undefined
+// -- we declare variable and not assigning value
+let undefined;
+console.log(undefined); // Output: undefined 
+
+// NaN
+// the boolean false
+// '', "", ``, empty string
+
